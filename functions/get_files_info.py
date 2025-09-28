4db6f339d1c2614e35a3e69f71fdd47fd661fc15
Ashutosh Pradhan <ashutosh.pradhan0049@gmail.com>
2025-09-28 23:24:31 +0530

    Function calling implemented.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,15 +1,17 @@
 import os
+from google.genai import types
 
 #  This is kind of a guard rail
-def get_files_info(working_directory, directory=None):
+def get_files_info(working_directory, directory="."):
     abs_working_dir = os.path.abspath(working_directory)
+    abs_directory = ""
     if directory is None:
-        directory = working_directory
         abs_directory = os.path.abspath(working_directory)
-    abs_directory = os.path.abspath(os.path.join(working_directory,directory))
+    else:
+        abs_directory = os.path.abspath(os.path.join(working_directory,directory))
 
     if not abs_directory.startswith(abs_working_dir):
-        return f'Error: "{directory}" is not a directory' # No exception: because llm is using this error
+        return f'Error: "{directory}" is not in working directory' # No exception: because llm is using this error
     
     final_response = ""
     contents = os.listdir(abs_directory)
@@ -20,3 +22,18 @@ def get_files_info(working_directory, directory=None):
         final_response += f"- {content}: file_size={size} bytes, is_dir={is_dir}\n"
 
     return final_response
+
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import sys
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.write_file import write_file, schema_write_file
+from functions.run_python_file import run_python_file, schema_run_python_file
+from call_function import call_function
 
 def main():
 
@@ -11,6 +15,19 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+- Read the content of a file
+- Write to a file (create or update)
+- Run a python file with optional arguments
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
     if len(sys.argv) < 2:
         print("We need a prompt")
@@ -24,11 +41,26 @@ def main():
     messages = [
     types.Content(role="user", parts=[types.Part(text=prompt)]),
 ]
+    
+    available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_run_python_file,
+        schema_write_file,
+        schema_get_file_content
+    ]
+)
+    
+    config=types.GenerateContentConfig(
+    tools=[available_functions], system_instruction=system_prompt
+)
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=messages )
- 
-    print(response.text)
+        model="gemini-2.0-flash-001", 
+        contents=messages,
+        config=config
+    )
+
     if response is None or response.usage_metadata is None:
         print("response is malformed")
         return
@@ -38,6 +70,13 @@ def main():
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-print(get_files_info("calculator", "pkg"))
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            result = call_function(function_call_part)
+            print(result)
+    else:
+        print(response.text)
+    
+    
 
-# main()
+main()
